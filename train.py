f6e2d76911acd0dd8f2280b9ac9b39b9ce24bfdd
MatthiasHerp <63145900+MatthiasHerp@users.noreply.github.com>
2020-09-01 11:52:58 +0200

    Update train.py

--- train.py
+++ train.py
@@ -296,11 +296,9 @@ if __name__ == "__main__":
    exogen = os.path.join(os.path.dirname(os.path.abspath(__file__)), "exogen_variables.csv")
    exogen = pd.read_csv(exogen, index_col='date')
     
-   
+   '''
    # Include days before and after events into the exogen data set
    exogen = days_around_events(exogen, before, after)
-
-'''
 
    # Define training and prediction data sets for the exogen variables
    exog_to_train = exogen.iloc[:(len(revenue_CA_1_FOODS_day) - 365)]
