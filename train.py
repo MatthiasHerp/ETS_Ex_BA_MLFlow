962bb16ea7821694a3d6f107f9e5e416945e516a
MatthiasHerp <63145900+MatthiasHerp@users.noreply.github.com>
2020-09-04 11:05:12 +0200

    Update train.py

--- train.py
+++ train.py
@@ -352,7 +352,7 @@ if __name__ == "__main__":
         #running the model optimization
         res = minimize(model, Starting_Parameters_optimal, args=(np.array(y['revenue']), exog_to_train), 
                        method='L-BFGS-B', bounds = bounds)
-'''
+
         #the fit extracter is run with the optimal values optained from the optimizer (res.x) and the time series y
         fit = fit_extracter(res.x, np.array(y['revenue']), exog_to_train)
 
@@ -495,5 +495,3 @@ if __name__ == "__main__":
         
 
         #mlflow.log_model(model, "ETS_Exogen")
-
-'''
