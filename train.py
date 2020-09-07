5810287428d632c4bee1575338f19aadac8d0b9a
MatthiasHerp <63145900+MatthiasHerp@users.noreply.github.com>
2020-09-07 12:36:34 +0200

    Update train.py

--- train.py
+++ train.py
@@ -648,9 +648,10 @@ if __name__ == "__main__":
         # into the new MLflow model's directory.
 
         artifacts = {
-            "exogen_variables": "/Users/mah/Desktop/M5_Wallmart_Challenge/exogen_variables.csv"
+            "exogen_variables": os.path.join(os.path.dirname(os.path.abspath(__file__)), "exogen_variables.csv")
         }
         #os.path.join(os.path.dirname(os.path.abspath(__file__)), "exogen_variables.csv")
+        #"/Users/mah/Desktop/M5_Wallmart_Challenge/exogen_variables.csv"
 
         #how do i set the directory so he understands where the file is? because it comes from a repository?
 
